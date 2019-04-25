470e29e5eb128618799e440f58aa8ac977ba27f2
Jakub <jackubek4@gmail.com>
2019-04-25 13:43:32 +0200

    Added klamerka

--- gawarit-gawarit/src/src/GUI.java
+++ gawarit-gawarit/src/src/GUI.java
@@ -4,14 +4,14 @@ import javax.swing.JPanel;
 
 
 
-public class GUI extends JPanel{
-	
-	GUI gui;
-	MainFrame mainFrame;
-	
-	
-	public GUI(MainFrame mainFrame) {
-	   	gui = this;
-		this.mainFrame = mainFrame;
-   }
-   
+public class GUI extends JPanel {
+
+    GUI gui;
+    MainFrame mainFrame;
+
+
+    public GUI(MainFrame mainFrame) {
+        gui = this;
+        this.mainFrame = mainFrame;
+    }
+}
