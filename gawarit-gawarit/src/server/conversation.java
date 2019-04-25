ccf2e3092294212f2a07b7dda05d6884d0c5a8f1
Jakub <jackubek4@gmail.com>
2019-04-25 14:28:42 +0200

    Important Commit test. Class Conversation created

---
+++ gawarit-gawarit/src/server/conversation.java
@@ -0,0 +1,15 @@
+package server;
+
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class conversation implements Runnable{
+
+    @Override
+    public void run() {
+
+    }
+}
