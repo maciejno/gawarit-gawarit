3411c344d1af155a0dbc2a4b94fb38e673517f40
Jakub <jackubek4@gmail.com>
2019-04-28 16:58:03 +0200

    Minor Changes

---
+++ gawarit-gawarit/src/server/User.java
@@ -0,0 +1,58 @@
+package server;
+
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
+
+
+public class User {
+
+    private String Username;
+    private String Password;
+    private List<String> Friends = new ArrayList<String>();
+
+    User(String login) {
+        InputStreamReader streamReader = null;
+        BufferedReader bufferedReader = null;
+        try {
+            InputStream inputStream = getClass().getResourceAsStream("/" + login + ".txt");
+            streamReader = new InputStreamReader(inputStream);
+            bufferedReader = new BufferedReader(streamReader);
+            String line;
+            line = bufferedReader.readLine();
+            Username = login;
+            Password = line;
+
+            while (true) {
+                line = bufferedReader.readLine();
+                if (line == null) break;
+                else Friends.add(line);
+            }
+        } catch (IOException e) {
+            System.err.println("Blad przy wczytaniu pliku");
+        }
+        try {
+            streamReader.close();
+            bufferedReader.close();
+        } catch (IOException e) {
+            System.err.println("BLAD PRZY ZAMYKANIU PLIKU!");
+            System.exit(3);
+        }
+
+    }
+
+    void PrintInfo() { //do debugowania
+        System.out.println("Username: " + Username );
+        System.out.println( "Password:" + Password );
+        System.out.println("Towarzysze " + Username + ":" );
+        for (String Friend : Friends)
+            System.out.println(Friend);
+    }
+
+    void RewriteUser() {
+
+    }
+
+}
